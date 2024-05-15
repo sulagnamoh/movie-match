d8276a9285c03fa1c51cac446b74e5f40b65f4ba
Anish-1101 <150402143+Anish-1101@users.noreply.github.com>
2024-05-14 22:12:38 -0700

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Learn more about what we do and our mission.</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to the Homepage</h1>
+            <p>This is the main page of our simple React application.</p>
+        </div>
+    );
+}
+
+export default Home;
