f09db28632c6cba03fd4ba6b15d92dd205d20cfb
Anish Reddy Athmakoor <anish@Anishs-MacBook.local>
2024-05-15 18:47:50 -0700

    homepage links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -7,45 +7,41 @@ import {
     Routes,
     Route,
     Link
-} from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
     return (
         <Router>
             <div>
-            <header className="header">
-      <h1>Movie Match</h1>
-      <h5>Your guide to finding your new favorite movies!</h5>
-      </header>
-                {/* <nav>
-                    <Link to="/Home">Home</Link> | 
-                    <Link to="/About">User Profile</Link>
+                <header className="header">
+                    <h1>Movie Match</h1>
+                    <h5>Your guide to finding your new favorite movies!</h5>
+                </header>
+                <nav>
+                    <Link to="/home">Home</Link> | 
+                    <Link to="/about">User Profile</Link>
                 </nav>
                 <Routes>
                     <Route path="/" element={<Home />} />  // Root path
-                    <Route path="/Home" element={<Home />} />
-                    <Route path="/About" element={<About />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/about" element={<About />} />
                     <Route path="*" element={<Home />} />  // Fallback to Home for undefined paths
-                </Routes> */}
+                </Routes>
                 <button>Search</button>
-            
-            <Dropdown className="d-inline mx-2" autoClose="outside">
-      <Dropdown.Toggle id="dropdown-autoclose-outside">
-       Filters
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#">Genre</Dropdown.Item>
-        <Dropdown.Item href="#">Most Popular</Dropdown.Item>
-        <Dropdown.Item href="#">idk</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-    </div>
+                <Dropdown className="d-inline mx-2" autoClose="outside">
+                    <Dropdown.Toggle id="dropdown-autoclose-outside">
+                        Filters
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#">Genre</Dropdown.Item>
+                        <Dropdown.Item href="#">Most Popular</Dropdown.Item>
+                        <Dropdown.Item href="#">idk</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
         </Router>
-        
-        
     );
 }
 
