43d2f051b9fd73f21d3e1294713ddfc7e423d5ee
Sulagna Mohanty <sulagnamoh123@g.ucla.edu>
2024-05-15 18:35:24 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Dropdown from 'react-bootstrap/Dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import {
     BrowserRouter as Router,
     Routes,
@@ -12,18 +15,37 @@ function App() {
     return (
         <Router>
             <div>
-                <nav>
+            <header className="header">
+      <h1>Movie Match</h1>
+      <h5>Your guide to finding your new favorite movies!</h5>
+      </header>
+                {/* <nav>
                     <Link to="/Home">Home</Link> | 
-                    <Link to="/About">About</Link>
+                    <Link to="/About">User Profile</Link>
                 </nav>
                 <Routes>
                     <Route path="/" element={<Home />} />  // Root path
                     <Route path="/Home" element={<Home />} />
                     <Route path="/About" element={<About />} />
                     <Route path="*" element={<Home />} />  // Fallback to Home for undefined paths
-                </Routes>
-            </div>
+                </Routes> */}
+                <button>Search</button>
+            
+            <Dropdown className="d-inline mx-2" autoClose="outside">
+      <Dropdown.Toggle id="dropdown-autoclose-outside">
+       Filters
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#">Genre</Dropdown.Item>
+        <Dropdown.Item href="#">Most Popular</Dropdown.Item>
+        <Dropdown.Item href="#">idk</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    </div>
         </Router>
+        
+        
     );
 }
 
